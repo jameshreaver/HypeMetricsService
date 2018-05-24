a7e079a7e0a8a284f07c547913a7998a903f0dc1
Giacomo Guerci <giacomo.guerci14@imperial.ac.uk>
2018-05-24 01:13:01 +0100

    Remove CORS headers

--- service.js
+++ service.js
@@ -6,14 +6,7 @@ const nedb = require('nedb');
 
 const app = express();
 const port = process.env.PORT || 80;
-
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 var db = new nedb({
   filename: './db/metrics.db',
